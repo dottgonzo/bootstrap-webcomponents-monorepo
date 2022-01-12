52ed6d021760044e2f0bcd53ed8f5ceb848d4d76
Dario Caruso <darioyzf@gmail.com>
2022-01-12 19:56:55 +0100

    up storybook

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t dottgonzo/htmlbricks-storybook-docs:v0.3.1 . && docker push dottgonzo/htmlbricks-storybook-docs:v0.3.1
+docker build -t dottgonzo/htmlbricks-storybook-docs:v0.3.2 . && docker push dottgonzo/htmlbricks-storybook-docs:v0.3.2
