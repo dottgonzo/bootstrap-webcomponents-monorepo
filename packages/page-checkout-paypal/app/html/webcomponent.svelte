216127a611570c3b0860cf17c924ae52a758495b
dottg0n20 <dev@dariocaruso.info>
2022-01-18 20:55:36 +0100

    re add page checkout

---
+++ packages/page-checkout-paypal/app/html/webcomponent.svelte
@@ -0,0 +1,65 @@
+<svelte:options tag="hb-page-checkout-paypal" />
+
+<script lang="ts">
+	/**
+	 * Svelte Payment Page WebComponent
+	 * =====================
+	 *
+	 * @contributors: Dario Caruso <dev@dariocaruso.info> (https://dariocaruso.info)
+	 *
+	 * @license: MIT License
+	 *
+	 */
+
+	import { createEventDispatcher } from "svelte";
+	import { get_current_component } from "svelte/internal";
+	import pkg from "../../package.json";
+	import type { IShipment, IUser } from "../../../checkout-paypal/app/types/webcomponent.type";
+	import type { ICartHeaders, IShopItem } from "../../../checkout-shopping-cart/app/types/webcomponent.type";
+	import type { FormSchema } from "../../../form/app/types/webcomponent.type";
+
+	export let id: string;
+	export let shipments: IShipment[];
+	export let user: IUser;
+	export let headers: ICartHeaders;
+	export let items: IShopItem[];
+	$: {
+		if (!id) id = null;
+		if (!shipments) shipments = null;
+		if (!headers) headers = null;
+		if (!items) items = null;
+
+		if (!user) user = null;
+	}
+
+	const component = get_current_component();
+	const svelteDispatch = createEventDispatcher();
+	function dispatch(name, detail) {
+		svelteDispatch(name, detail);
+		component.dispatchEvent && component.dispatchEvent(new CustomEvent(name, { detail }));
+	}
+	function addComponent(componentName: string) {
+		if (!document.getElementById("hb-" + componentName + "-script")) {
+			const script = document.createElement("script");
+			script.id = "hb-" + componentName + "-script";
+			script.src = `https://cdn.jsdelivr.net/npm/@htmlbricks/hb-${componentName}@${pkg.version}/release/release.js`;
+			if (location.href.includes("localhost")) script.src = `http://localhost:6006/${componentName}/dist/release.js`;
+
+			document.head.appendChild(script);
+		}
+	}
+	addComponent("checkout-shopping-cart");
+	addComponent("checkout-paypal");
+</script>
+
+<div class="container">
+	<div class="row">
+		<div class="col-7 section"><hb-checkout-paypal style="margin-right:30px" {user} {shipments} /></div>
+		<div class="col-5 section"><hb-checkout-shopping-cart style="margin-left:30px" {items} {headers} /></div>
+	</div>
+</div>
+
+<style lang="scss">
+	@import "../styles/bootstrap.scss";
+	@import "../styles/webcomponent.scss";
+</style>
---
+++ storybook/stories/PageCheckoutPaypal.stories.ts
@@ -0,0 +1,44 @@
+import { Story, Meta } from "@storybook/html";
+import { webComponentBind } from "./utils/webComponentUtils";
+
+const meta: Meta = {
+  title: "Pages/PageCheckoutPaypal",
+
+  argTypes: {
+    id: { control: { disable: true } },
+    shipments: { control: { type: "array" } },
+    items: { control: { type: "array" } },
+    user: { control: { type: "object" } },
+  },
+};
+
+export default meta;
+
+const Template: Story = (args) =>
+  webComponentBind(args, meta.argTypes, "page-checkout-paypal");
+
+export const PageCheckoutPaypal = Template.bind({});
+
+PageCheckoutPaypal.args = {
+  id: "PageCheckoutPaypal",
+  shipments: [
+    {
+      price: 10,
+
+      durationInSeconds: 0,
+      available: true,
+    },
+  ],
+  items: [
+    {
+      unitaryPrice: 2,
+      taxPercentage: 3,
+      name: "testitem",
+    },
+    {
+      unitaryPrice: 5,
+      taxPercentage: 7,
+      name: "testitem2",
+    },
+  ],
+};
