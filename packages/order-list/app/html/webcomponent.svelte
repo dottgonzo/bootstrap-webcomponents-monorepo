8a2143e1863f33ed1aa2b76f3ce9e30931ad51f2
dottg0n20 <dev@dariocaruso.info>
2022-01-28 09:40:57 +0100

    add order list

---
+++ packages/order-list/app/html/webcomponent.svelte
@@ -0,0 +1,43 @@
+<svelte:options tag="hb-order-list" />
+
+<script lang="ts">
+	/**
+	 * Svelte Payment Page WebComponent
+	 * =====================
+	 *
+	 * @contributors: Dario Caruso <dev@dariocaruso.info> (https://dariocaruso.info)
+	 *
+	 * @license: MIT License
+	 *
+	 */
+
+	import { createEventDispatcher } from "svelte";
+	import { get_current_component } from "svelte/internal";
+	import pkg from "../../package.json";
+
+	export let id: string;
+
+	$: {
+		if (!id) id = null;
+	}
+
+	const component = get_current_component();
+	const svelteDispatch = createEventDispatcher();
+	function dispatch(name, detail) {
+		svelteDispatch(name, detail);
+		component.dispatchEvent && component.dispatchEvent(new CustomEvent(name, { detail }));
+	}
+</script>
+
+<!-- {#if completed ==="yes"}
+				<div>Checkout status:</div>
+			{:else}
+				<div>Checkout status:</div>
+			{/if} -->
+<div>todo</div>
+
+<style lang="scss">
+	:host {
+		font-family: "Roboto", sans-serif;
+	}
+</style>
