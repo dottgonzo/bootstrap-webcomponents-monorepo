a26f2b5232dd86e1881239630005a308e2ea5087
dottg0n20 <dev@dariocaruso.info>
2022-02-17 01:40:53 +0100

    ref

--- packages/jsutils/main.js
+++ packages/jsutils/main.js
@@ -1,8 +1,8 @@
-export function addComponent(componentName, allowLocal) {
+export function addComponent(componentName, version, allowLocal) {
     if (!document.getElementById("hb-" + componentName + "-script")) {
         const script = document.createElement("script");
         script.id = "hb-" + componentName + "-script";
-        script.src = `https://cdn.jsdelivr.net/npm/@htmlbricks/hb-${componentName}@${pkg.version}/release/release.js`;
+        script.src = `https://cdn.jsdelivr.net/npm/@htmlbricks/hb-${componentName}@${version}/release/release.js`;
         if (allowLocal && location.href.includes("localhost")) script.src = `http://localhost:6006/${componentName}/dist/release.js`;
 
         document.head.appendChild(script);
