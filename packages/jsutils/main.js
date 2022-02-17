79cc28cf4f99580b1a6ba9d0d9345c2877daab85
dottg0n20 <dev@dariocaruso.info>
2022-02-17 01:54:53 +0100

    add ts on utils

--- packages/jsutils/main.js
+++ packages/jsutils/main.js
@@ -1,10 +1,15 @@
-export function addComponent(componentName, version, allowLocal) {
+"use strict";
+exports.__esModule = true;
+exports.addComponent = void 0;
+function addComponent(componentName, version, allowLocal) {
     if (!document.getElementById("hb-" + componentName + "-script")) {
-        const script = document.createElement("script");
+        var script = document.createElement("script");
         script.id = "hb-" + componentName + "-script";
-        script.src = `https://cdn.jsdelivr.net/npm/@htmlbricks/hb-${componentName}@${version}/release/release.js`;
-        if (allowLocal && location.href.includes("localhost")) script.src = `http://localhost:6006/${componentName}/dist/release.js`;
-
+        script.src = "https://cdn.jsdelivr.net/npm/@htmlbricks/hb-".concat(componentName, "@").concat(version, "/release/release.js");
+        if (allowLocal && location.href.includes("localhost"))
+            script.src = "http://localhost:6006/".concat(componentName, "/dist/release.js");
         document.head.appendChild(script);
     }
 }
+exports.addComponent = addComponent;
+//# sourceMappingURL=main.js.map
