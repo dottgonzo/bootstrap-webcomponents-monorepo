b96bf5b2956e5ab3fa497653f7d7c3d819fac073
dottg0n20 <dev@dariocaruso.info>
2022-03-04 21:00:37 +0100

    fix area-code

--- packages/area-code/app/types/webcomponent.type.d.ts
+++ packages/area-code/app/types/webcomponent.type.d.ts
@@ -1,23 +1,3 @@
 export type Component = {
-	type: "login" | "register" | "activate" | "recover" | "forgetpassword";
-
-	language: string;
-
-	sessionkey: string;
-
-	redirectonlogin: string;
-	redirectoncreate: string;
-	loginuri: string;
-	registeruri: string;
-	activateuri: string;
-	recoveruri: string;
-	requestmethod: string;
-	appendqueryparams: string;
-	appendbodyparams: string;
-	logouri: string;
-	oauth2providers: string;
-	disableregister: boolean;
-
-	passwordpattern: string;
-	recoverycode: string;
+	content: string;
 };
