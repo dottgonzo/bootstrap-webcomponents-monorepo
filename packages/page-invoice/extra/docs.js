758d1c97434ca2df63396e45ba5bce384d00468f
dottg0n20 <dev@dariocaruso.info>
2022-03-05 18:18:54 +0100

    remove compiled js

--- packages/page-invoice/extra/docs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.storybookArgs = void 0;
-exports.storybookArgs = {
-    printer: { control: { type: "text" } },
-    headers: { control: { type: "object" } },
-    items: { control: { type: "array" } }
-};
