74eca02550a39040e8520a9afa976b244bc6bf91
Dario Caruso <darioyzf@gmail.com>
2022-03-08 19:41:02 +0100

    upp

--- site/src/components/controlTable.svelte
+++ site/src/components/controlTable.svelte
@@ -19,7 +19,28 @@
 					{#if storybookargs[prop]?.control?.type === 'text'}
 						<input type="text" bind:value={args[prop]} />
 					{:else if storybookargs[prop]?.control?.type === 'boolean'}
-						bool
+						<select
+							on:change={(c) => {
+								const val = c.target['value'];
+								args[prop] = val === 'yes' ? true : false;
+								console.log('changeme', val);
+							}}
+							value={args[prop] ? 'yes' : 'no'}
+						>
+							<option value="yes">yes</option>
+							<option value="no">no</option>
+						</select>
+					{:else if ['object', 'array'].includes(storybookargs[prop]?.control?.type)}
+						<input
+							type="text"
+							on:input={(c) => {
+								const val = c.target['value'];
+								args[prop] = JSON.parse(val);
+
+								console.log('changemew', c);
+							}}
+							value={JSON.stringify(args[prop])}
+						/>
 					{/if}
 				</td>
 			</tr>
