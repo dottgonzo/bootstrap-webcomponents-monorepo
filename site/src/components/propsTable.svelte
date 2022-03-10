21da3b20bce304e188e6b898aa36420cb95459d1
dottg0n20 <dev@dariocaruso.info>
2022-03-10 01:24:34 +0100

    up packages

--- site/src/components/propsTable.svelte
+++ site/src/components/propsTable.svelte
@@ -1,6 +1,50 @@
 <script lang="ts">
 	export let definition: any;
 	export let storybookargs: any;
+
+	function defToInterfaceString(definition: {
+		properties: any;
+		type: 'object' | 'string' | 'array';
+		$ref?: string;
+	}) {
+		if (definition.$ref) return definition.$ref.replace('#/definitions/', '');
+		if (definition.type === 'object') {
+			let text = '{';
+			for (const propKey of Object.keys(definition.properties)) {
+				const prop = definition.properties[propKey];
+				text += ' ' + propKey + ':';
+				if (prop)
+					switch (prop.type) {
+						case 'array':
+							if (prop.items) text += defToInterfaceString(prop.items) + '[ ],';
+							else text += '[ ],';
+						case 'object':
+							// if (propDefinition.items) return defToInterfaceString(propDefinition.items) + '[ ]';
+							text += '{ },';
+						default:
+							text += prop.type + ',';
+					}
+			}
+			text += '}';
+			return text.replace(',}', '}');
+		}
+	}
+
+	function propToType(prop: string) {
+		const propDefinition = definition.definitions.Component.properties[prop];
+		if (propDefinition.$ref) return propDefinition.$ref.replace('#/definitions/', '');
+
+		switch (propDefinition.type) {
+			case 'array':
+				if (propDefinition.items) return defToInterfaceString(propDefinition.items) + '[ ]';
+				return '[ ]';
+			case 'object':
+				// if (propDefinition.items) return defToInterfaceString(propDefinition.items) + '[ ]';
+				return '{}';
+			default:
+				return propDefinition.type;
+		}
+	}
 </script>
 
 <table style="width:100%">
@@ -14,7 +58,7 @@
 	{#each Object.keys(definition.definitions.Component.properties) as prop (prop)}
 		<tr>
 			<td>{prop}</td>
-			<td>{definition.definitions.Component.properties[prop].type}</td>
+			<td>{propToType(prop)}</td>
 			<td>{definition.definitions.Component.required?.includes(prop) ? 'true' : 'false'}</td>
 			<td>{storybookargs[prop]?.defaultValue || ''}</td>
 		</tr>
