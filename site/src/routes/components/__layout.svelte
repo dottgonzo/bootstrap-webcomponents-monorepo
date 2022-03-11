21d9479dd863fb7792e1e8966032de4b1ca4d841
Dario Caruso <darioyzf@gmail.com>
2022-03-11 20:14:49 +0100

    added bootstrap

--- site/src/routes/components/__layout.svelte
+++ site/src/routes/components/__layout.svelte
@@ -117,6 +117,18 @@
 	}
 </script>
 
+<svelte:head>
+	<!-- <link
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+		rel="stylesheet"
+		integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+		crossorigin="anonymous"
+	/> -->
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+	/>
+</svelte:head>
 <hb-layout
 	pagename={$pageName}
 	companytitle="fff"
@@ -145,7 +157,25 @@
 	socials={JSON.stringify({ facebook: 'fbbb', youtube: 'yttttttt' })}
 	on:pagechange={(e) => pageChange(e.detail)}
 >
-	<div style="margin:auto;max-width:1000px" slot="page">
+	<div id="sveltelay" style="margin:auto;max-width:1000px" slot="page">
 		<slot />
 	</div>
 </hb-layout>
+
+<style lang="scss" global>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+	@import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css');
+	$primary: #07689f;
+	$secondary: #c9d6df;
+	$success: #11d3bc;
+	$danger: #f67280;
+	$info: #a2d5f2;
+	$warning: #ff7e67;
+	$light: #fafafa;
+	$dark: #4e4e4e;
+	@import 'bootstrap/scss/bootstrap';
+
+	#sveltelay {
+		font-family: 'Roboto', sans-serif;
+	}
+</style>
