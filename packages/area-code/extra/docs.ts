6985b15d542e73483d83131f51a21d890e872e6a
dottg0n20 <dev@dariocaruso.info>
2022-03-13 04:58:36 +0100

    better areaCodeStorybookArgs

--- packages/area-code/extra/docs.ts
+++ packages/area-code/extra/docs.ts
@@ -2,6 +2,7 @@ import type { CssPart, CssVar, HtmlSlot } from "@htmlbricks/hb-jsutils/main";
 
 export const storybookArgs = {
 	content: { control: { type: "text" } },
+	clipboardCopyText: { action: "clipboardCopyTextEvent" },
 };
 
 export const cssVars: CssVar[] = [];
