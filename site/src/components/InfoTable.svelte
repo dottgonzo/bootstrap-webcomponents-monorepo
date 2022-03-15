c1d9d45c4facae2563ec49c42a011ae4e85967fa
Dario Caruso <darioyzf@gmail.com>
2022-03-16 00:42:26 +0100

    test readme

--- site/src/components/InfoTable.svelte
+++ site/src/components/InfoTable.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import MarkdownIt from 'markdown-it';
+	import { pageName } from '../stores/app';
+
 	// export let definition: any;
 	// export let storybookargs: any;
 	// export let args: any;
+	let content;
+	async function fetchInfo() {
+		content = 'loading';
+		try {
+			const pageraw = await fetch(
+				`https://cdn.jsdelivr.net/gh/dottgonzo/bootstrap-webcomponents-monorepo@latest/packages/${$pageName}/README.md`
+			);
+			const txt = await pageraw.text();
+			const md = new MarkdownIt();
+
+			content = md.render(txt);
+		} catch (err) {
+			console.warn(`no readme page for ${$pageName}`);
+		}
+	}
+	$: {
+		if ($pageName) fetchInfo().catch(console.error);
+	}
+	// onMount(() => {
+	// });
 </script>
 
-blabla blabla
+{@html content}
