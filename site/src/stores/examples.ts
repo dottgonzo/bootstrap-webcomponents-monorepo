0313c4899c7f93e5d61eada41e276a43689a3957
Dario Caruso <darioyzf@gmail.com>
2022-03-15 22:03:14 +0100

    copy all examples

--- site/src/stores/examples.ts
+++ site/src/stores/examples.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-
+import dayjs from 'dayjs';
 const paginateExampleValues = {
 	page: 2,
 	pages: 4
@@ -7,7 +7,6 @@ const paginateExampleValues = {
 
 const inputTextExampleValues = {
 	schemaentry: {
-		type: 'text',
 		placeholder: 'Insert your last name here...',
 		id: 'lastName',
 		required: true,
@@ -209,46 +208,728 @@ const offcanvasExampleValues = {
 	navpage: 'settings',
 	companytitle: 'acme corp'
 };
+const events = [
+	{
+		date: new Date(),
+		id: 'test',
+		label: 'thetest'
+	},
+	{
+		date: dayjs().startOf('month').subtract(1, 'day').toDate(),
+		id: 'test2',
+		label: 'thetest start'
+	}
+];
 
-const authExampleValues = {};
-const calendarEventsExampleValues = {};
-const cardVideoExampleValues = {};
-const chartjsExampleValues = {};
-const checkoutExampleValues = {};
-const checkoutShoppingCartExampleValues = {};
-const contactItemExampleValues = {};
-const cookieLawBannerExampleValues = {};
-const dialogExampleValues = {};
+const paypal = {
+	id: 'paypal',
+	label: 'bbbb',
+	paypalid: 'test'
+};
+
+const defaultPayment = {
+	merchantName: 'testmerchant',
+	countryCode: 'it',
+	currencyCode: 'EUR',
+	total: 45,
+	type: 'buy'
+};
+const schema1 = [
+	{
+		id: 'name-row',
+		type: 'row',
+		params: {
+			columns: [
+				{
+					type: 'text',
+					placeholder: 'Insert your first name here...',
+					id: 'firstName',
+					required: true,
+					label: 'First Name',
+					validationTip: 'This field cannot be empty.'
+				},
+				{
+					type: 'text',
+					placeholder: 'Insert your last name here...',
+					id: 'lastName',
+					required: true,
+					label: 'Last Name',
+					validationTip: 'This field cannot be empty.'
+				},
+				{
+					type: 'text',
+					placeholder: 'Insert your last namez here...',
+					id: 'lastNamez',
+					label: 'Last Namez',
+					validationTip: 'This field cannot be emptyz.'
+				},
+				{
+					type: 'select',
+					placeholder: 'Select something here...',
+					id: 'selectsomething',
+					required: true,
+					label: 'Selection of something',
+					validationTip: 'This field cannot be empty.',
+					params: {
+						options: [
+							{ label: '', value: '' },
+							{ label: 'testlabel', value: 'testvalue' }
+						]
+					}
+				},
+				{
+					type: 'radio',
+					placeholder: 'radio something here...',
+					id: 'radiosomething',
+					required: true,
+					label: 'Selection of something',
+					validationTip: 'This field cannot be empty.',
+					params: {
+						options: [
+							{ label: 'radio', value: 'radio' },
+							{ label: 'radio2', value: 'radi2o' },
+							{ label: 'radio3', value: 'radio2' },
+							{ label: 'testlabel', value: 'testvalue' }
+						]
+					}
+				}
+			]
+		}
+	},
+	{
+		type: 'number',
+		id: 'age',
+		required: true,
+		label: 'Age',
+		params: {
+			min: 8,
+			max: 120
+		},
+		validationTip: 'Min 8, Max 120'
+	},
+	{
+		type: 'text',
+		placeholder: 'Insert your last name here...',
+		id: 'lastName2',
+		required: true,
+		label: 'Last Name',
+		validationTip: 'This field cannot be empty.'
+	},
+	{
+		type: 'textarea',
+		placeholder: 'Insert your last name22 here...',
+		id: 'lastName22',
+		required: true,
+		label: 'Last Name2',
+		validationTip: 'This field cannot be empty2.'
+	}
+];
+const preferences = [
+	{
+		type: 'text',
+		placeholder: 'Inserisci titolo di default',
+		id: 'defaultTitle',
+		required: true,
+
+		label: 'Titolo di default',
+		value: 'numtest',
+		params: {
+			minlength: 8,
+			maxlength: 120
+		}
+	},
+	{
+		type: 'number',
+		id: 'age',
+		required: true,
+		label: 'Age',
+		value: 9,
+		params: {
+			min: 8,
+			max: 120
+		},
+		validationTip: 'Min 8, Max 120'
+	}
+];
+const preferences2 = [
+	{
+		type: 'text',
+		placeholder: 'Inserisci titolo di default2',
+		id: 'defaultTitle2',
+		required: true,
+
+		label: 'Titolo di default2',
+		value: 'ntest2',
+		params: {
+			minlength: 8,
+			maxlength: 120
+		}
+	},
+	{
+		type: 'number',
+		id: 'age2',
+		required: true,
+		label: 'Age2',
+		value: 20,
+		params: {
+			min: 8,
+			max: 120
+		},
+		validationTip: 'Min 8, Max 120'
+	}
+];
+const cards = [
+	{
+		title: 'bau',
+		videoSrc: 'https://comapi.be-on.tv/v1/public/records/61043eedb50af916a6429fdc/video',
+		time: '2021-08-06T22:46:30.565Z'
+	},
+	{
+		title: 'bb1',
+		videoSrc: 'https://comapi.be-on.tv/v1/public/records/61043eedb50af916a6429fdc/video',
+		time: '2021-08-06T22:46:31.565Z'
+	},
+	{ title: 'bb2', time: '2021-08-02T22:46:32.565Z' },
+	{
+		title: 'ss',
+		time: '2021-08-06T22:46:34.565Z',
+		description: 'fffffff',
+		testnested: { nested: 'dddd' }
+	},
+	{
+		title: 'bb4',
+		time: '2021-08-06T22:46:34.565Z',
+		videoSrc: 'https://www.youtube.com/embed/tgbNymZ7vqY',
+		provider: 'youtube'
+	},
+	{ title: 'aa5', time: '2021-08-06T22:46:34.565Z' },
+	{ title: 'bb6', time: '2021-08-06T22:46:34.565Z' },
+	{ title: 'bb7', time: '2021-08-06T22:46:34.565Z' },
+	{ title: 'bb8', time: '2021-08-06T22:46:34.565Z' },
+	{ title: 'bb9', time: '2021-08-06T22:46:33.565Z' },
+	{ title: 'bb10', time: '2021-08-06T22:46:34.565Z' },
+	{ title: 'bb11', time: '2021-08-06T22:46:34.565Z' },
+	{
+		title: 'bb12',
+		time: '2021-08-06T22:46:34.565Z',
+		videoSrc: 'https://comapi.be-on.tv/v1/public/records/61043eedb50af916a6429fdc/video'
+	},
+	{ title: 'bb13', time: '2021-08-07T22:46:39.565Z' }
+];
+const headers = [
+	{
+		label: 'title',
+		key: 'title',
+		search: true
+	},
+	{
+		label: 'description',
+		key: 'description'
+	},
+	{
+		label: 'nested',
+		key: 'testnested.nested'
+	},
+	{
+		label: 'data',
+		key: 'time',
+		type: 'datetime',
+		format: 'DD MMMM YYYY'
+	}
+];
+const defaultgw = {
+	id: 'paypal',
+	label: 'bbbb',
+	paypalid: 'test'
+};
+const googlepay = {
+	id: 'google',
+	label: 'GooglePay',
+	gatewayId: 'example',
+	gatewayMerchantId: 'exampleGatewayMerchantId'
+};
+const authExampleValues = {
+	type: 'login',
+	logouri: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Free_Content_Logo.svg'
+};
+
+const calendarEventsExampleValues = {
+	events
+};
+
+const cardVideoExampleValues = {
+	videosrc: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+	title: 'aa',
+	description: 'desc6',
+	time: '2021-08-15T18:55:22.135Z'
+};
+const chartjsExampleValues = {
+	data: {
+		type: 'line',
+		data: {
+			labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+			datasets: [
+				{
+					label: 'My First dataset',
+					backgroundColor: 'rgb(255, 99, 132)',
+					borderColor: 'rgb(255, 99, 132)',
+					data: [0, 10, 5, 2, 20, 30, 45]
+				}
+			]
+		},
+		options: {}
+	}
+};
+const checkoutExampleValues = {
+	shipments: [
+		{
+			price: 10,
+			currency: '€',
+
+			arriveDate: '2022-01-20T08:03:57.562Z',
+			available: true,
+			label: 'zio barca',
+			id: 'ziobarca'
+		},
+		{
+			price: 20,
+			currency: '€',
+
+			arriveDate: '2022-01-21T02:22:57.562Z',
+			available: true,
+			label: 'zio barca2',
+			id: 'ziobarca2'
+		}
+	],
+	gateways: [paypal],
+	payment: defaultPayment
+};
+const checkoutShoppingCartExampleValues = {
+	payment: {
+		items: [
+			{
+				unitaryPrice: 2,
+				taxPercentage: 3,
+				name: 'testitem'
+			},
+			{
+				unitaryPrice: 5,
+				taxPercentage: 7,
+				name: 'testitem2'
+			}
+		]
+	}
+};
+const contactItemExampleValues = {
+	email: { address: 'dev@dariocaruso.info' }
+};
+const cookieLawBannerExampleValues = {
+	allowdecline: true
+};
+const dialogExampleValues = {
+	show: true
+};
 const downloaderExampleValues = {};
-const dropdownSimpleExampleValues = {};
+const dropdownSimpleExampleValues = {
+	list: [
+		{
+			key: 'ciao',
+			label: 'bao'
+		},
+		{
+			key: 'ciao2',
+			label: 'bao2'
+		},
+		{
+			key: 'ciao3',
+			label: 'bao3',
+			linkHref: 'fefeef'
+		}
+	]
+};
 const editorTinymceExampleValues = {};
 const editorVideoExampleValues = {};
-const formExampleValues = {};
-const funnelExampleValues = {};
-const galleryVideoExampleValues = {};
-const gaugeExampleValues = {};
-const inputAreaExampleValues = {};
+const formExampleValues = {
+	schema: schema1
+};
+const funnelExampleValues = {
+	schemes: [preferences, preferences2]
+};
+const galleryVideoExampleValues = {
+	cards,
+	headers
+};
+const gaugeExampleValues = {
+	options: {
+		value: 50,
+		min: 0,
+		max: 100
+	}
+};
+const inputAreaExampleValues = {
+	schemaentry: {
+		placeholder: 'Insert your last name here...',
+		id: 'lastName',
+		required: true,
+		label: 'Last Name Area',
+		validationTip: 'This field cannot be empty.'
+	}
+};
 const inputCheckboxExampleValues = {};
 const inputDateExampleValues = {};
 const inputEmailExampleValues = {};
-const inputFileExampleValues = {};
+const inputFileExampleValues = {
+	placeholder: 'Insert your last name here...',
+	id: 'lastName',
+	required: true,
+	label: 'Last Name',
+	validationTip: 'This field cannot be empty.'
+};
 const inputNumberExampleValues = {};
-const inputRadioExampleValues = {};
+const inputRadioExampleValues = {
+	placeholder: 'Choose here...',
+	id: 'check0',
+	required: true,
+	label: 'Check 0',
+	validationTip: 'Check 1',
+	group: 'ciao',
+	params: {
+		options: [
+			{ label: '777', value: '555' },
+			{ label: 'testlabel', value: 'testvalue' }
+		]
+	}
+};
 const inputSelectExampleValues = {};
-const mapExampleValues = {};
-const modalVideoExampleValues = {};
-const navbarExampleValues = {};
-const orderListExampleValues = {};
-const pageCheckoutExampleValues = {};
-const pageInvoiceExampleValues = {};
-const pageLoginregisterExampleValues = {};
-const paymentPaypalExampleValues = {};
+const mapExampleValues = {
+	center: [10, 10],
+	zoom: 9,
+	source: { type: 'osm' },
+	options: { centerFromGeometries: true },
+	data: [
+		{
+			marker: {
+				lngLat: [10.1, 10],
+				icon: {
+					uri: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Visual_Studio_Code_Insiders_1.36_icon.svg',
+					scale: 0.15,
+					anchor: [0.5, 0.1]
+				},
+				popupHtml: '<p>You clicked here:</p><code>ciao</code>'
+			}
+		},
+		{
+			marker: {
+				lngLat: [10, 10],
+				popupHtml: '<p>You clicked here:</p><code>ciao</code>'
+			}
+		},
+		{
+			marker: {
+				lngLat: [10.3, 10]
+			}
+		}
+	]
+};
+const modalVideoExampleValues = {
+	uri: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4'
+};
+const navbarExampleValues = {
+	companybrandname: 'testbrand2',
+	pagetitle: 'pagetest'
+};
+const orderListExampleValues = {
+	payment: {
+		orderNumber: 'ssss',
+
+		items: [
+			{
+				id: 'sss',
+				unitaryPrice: 2,
+				taxPercentage: 3,
+				name: 'testitem',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/6/66/Walgreens_Obama_Merchandise.jpg'
+			},
+			{
+				id: 'aaa',
+				unitaryPrice: 5,
+				taxPercentage: 7,
+				name: 'testitem2',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Fredmeyer_edit_1.jpg'
+			}
+		]
+	}
+};
+const pageCheckoutExampleValues = {
+	shipments: [],
+	user: {
+		fullName: 'fdfff fffff',
+		addressWithNumber: 'addreess 43',
+		city: 'ff',
+		nationality: 'effe',
+		zip: '3434',
+		fixed: true
+	},
+	gateways: [defaultgw, googlepay],
+	payment: {
+		countryCode: 'IT',
+		items: [
+			{
+				unitaryPrice: 2,
+				taxPercentage: 3,
+				name: 'testitem'
+			},
+			{
+				unitaryPrice: 5,
+				taxPercentage: 7,
+				name: 'testitem2'
+			}
+		]
+	}
+};
+const pageInvoiceExampleValues = {
+	headers: {
+		serial: 'seriale1',
+		from: {
+			piva: 'piva',
+			name: 'companyfrom',
+			address: 'address',
+			email: 'email',
+			phone: 'phone',
+			logo: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Free_Content_Logo.svg',
+			shortName: 'Acme SpA'
+		},
+		to: {
+			piva: 'to iva',
+			name: 'to name',
+			address: 'to address',
+			email: 'to@email.com',
+			phone: 'tototo'
+		}
+	},
+	items: [
+		{
+			unitaryPrice: 2,
+			taxPercentage: 3,
+			desc: 'testitem'
+		},
+		{
+			unitaryPrice: 5,
+			taxPercentage: 7,
+			desc: 'testitem2'
+		}
+	]
+};
+const pageLoginregisterExampleValues = {
+	type: 'login',
+	company: company1
+};
+const paymentPaypalExampleValues = {
+	label: 'bbbb',
+	paypalid: 'test'
+};
 const playerLiveExampleValues = {};
-const productComparisonExampleValues = {};
-const rangeSliderExampleValues = {};
+const productComparisonExampleValues = {
+	headers: [
+		{
+			id: 'char1',
+			label: 'labello'
+		},
+		{
+			id: 'char2',
+			label: 'labello2'
+		},
+		{
+			id: 'char3',
+			label: 'labello3'
+		}
+	],
+	products: [
+		{
+			id: 'prrr',
+			price: 19,
+			model: 'starter',
+			description: 'weekly',
+			note: 'rotto',
+			characteristics: {
+				char1: 'valid',
+				char2: 'disabled',
+				char3: 'blocked',
+				char4: 'valid'
+			}
+		},
+		{
+			id: 'prrr2',
+			price: 49,
+			model: 'professional',
+			description: 'weekly',
+			note: 'rotto',
+			characteristics: {
+				char1: 'valid',
+				char2: 'disabled',
+				char3: 'blocked',
+				char4: 'valid'
+			}
+		},
+		{
+			id: 'prrr3',
+			price: 99,
+			model: 'advanced',
+			description: 'weekly',
+			note: 'rotto',
+			characteristics: {
+				char1: 'valid',
+				char2: 'disabled',
+				char3: 'blocked',
+				char4: 'valid'
+			}
+		},
+		{
+			id: 'prrr4',
+			price: 199,
+			model: 'custom',
+			description: 'weekly',
+			note: 'rotto',
+
+			characteristics: {
+				char1: 'valid',
+				char2: 'disabled',
+				char3: 'blocked',
+				char4: 'valid'
+			}
+		}
+	],
+	options: { currency: '€' }
+};
+const rangeSliderExampleValues = {
+	withbubbles: true,
+	minval: 20,
+	maxval: 60
+};
 const sidenavLinkExampleValues = {};
-const tableExampleValues = {};
-const toastExampleValues = {};
+const tableExampleValues = {
+	headers: [
+		{
+			label: 'title',
+			key: 'title',
+			search: true
+		},
+		{
+			label: 'description',
+			key: 'description'
+		},
+		{
+			label: 'nested',
+			key: 'testnested.nested'
+		},
+		{
+			label: 'data',
+			key: 'time',
+			type: 'datetime',
+			format: 'DD MMMM YYYY'
+		}
+	],
+	rows: [
+		{
+			title: 'bau',
+			videoSrc: 'https://comapi.be-on.tv/v1/public/records/61043eedb50af916a6429fdc/video',
+			time: '2021-08-06T22:46:30.565Z',
+			_id: 'bau',
+			status: 'closed'
+		},
+		{
+			title: 'bb1',
+			videoSrc: 'https://comapi.be-on.tv/v1/public/records/61043eedb50af916a6429fdc/video',
+			time: '2021-08-06T22:46:31.565Z',
+			description: 'ffff2',
+			_id: 'bb1',
+			status: 'closed'
+		},
+		{
+			title: 'bb2',
+			time: '2021-08-02T22:46:32.565Z',
+			_id: 'bb2',
+			status: 'active'
+		},
+		{
+			title: 'ss',
+			time: '2021-08-06T22:46:34.565Z',
+			description: 'fffffff',
+			testnested: { nested: 'dddd' },
+			_id: 'ss',
+			status: 'active'
+		},
+		{
+			title: 'bb4',
+			time: '2021-08-06T22:46:34.565Z',
+			_id: 'bb4',
+			status: 'active'
+		},
+		{
+			title: 'aa5',
+			time: '2021-08-06T22:46:34.565Z',
+			_id: 'aa5',
+			status: 'active'
+		},
+		{
+			title: 'bb6',
+			time: '2021-08-06T22:46:34.565Z',
+			_id: 'bb6',
+			status: 'active'
+		},
+		{
+			title: 'bb7',
+			time: '2021-08-06T22:46:34.565Z',
+			_id: 'bb7',
+			status: 'active'
+		},
+		{
+			title: 'bb8',
+			time: '2021-08-06T22:46:34.565Z',
+			_id: 'bb8',
+			status: 'active'
+		},
+		{
+			title: 'bb9',
+			time: '2021-08-06T22:46:33.565Z',
+			_id: 'bb9',
+			status: 'active'
+		},
+
+		{
+			title: 'bb10',
+			time: '2021-08-06T22:46:34.565Z',
+			_id: 'bb10',
+			status: 'active'
+		},
+		{
+			title: 'bb11',
+			time: '2021-08-06T22:46:34.565Z',
+			_id: 'bb11',
+			status: 'active'
+		},
+		{
+			title: 'bb12',
+			time: '2021-08-06T22:46:34.565Z',
+			videoSrc: 'https://comapi.be-on.tv/v1/public/records/61043eedb50af916a6429fdc/video',
+			_id: 'bb12',
+			status: 'active'
+		},
+		{
+			title: 'bb13',
+			time: '2021-08-07T22:46:39.565Z',
+			_id: 'bb13',
+			status: 'active'
+		}
+	]
+};
+const toastExampleValues = {
+	small: '11 mins ago',
+	title: 'Bootstrap',
+	show: true,
+	content: 'Hello, world! This is a toast message.',
+	img: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e7e03606d%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e7e03606d%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.4296875%22%20y%3D%22104.5%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E'
+};
 
 export const allComponentsExampleValues = writable({
 	banner: bannerExampleValues,
