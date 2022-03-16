f91ebe93edb475c6ac4a579087df314518ac3369
Dario Caruso <darioyzf@gmail.com>
2022-03-16 12:32:25 +0100

    add i18n utility

--- packages/jsutils/main.ts
+++ packages/jsutils/main.ts
@@ -31,3 +31,55 @@ export function addComponent(
     document.head.appendChild(script);
   }
 }
+
+export function getDefaultLang() {
+  let browserLang = "en";
+  if (
+    navigator?.languages &&
+    navigator.languages[0]?.split("-")[0]?.toLowerCase()?.length
+  ) {
+    browserLang = navigator.languages[0]?.split("-")[0]?.toLowerCase();
+  }
+  return browserLang;
+}
+
+export function getDictionaryWord(
+  wordKey: string,
+  dictionary: { [x: string]: { [x: string]: string } },
+  lang?: string
+) {
+  if (!wordKey) throw new Error("no wordKey provided");
+  if (!dictionary) throw new Error("no dictionary provided");
+
+  if (lang && dictionary[lang]?.[wordKey]) return dictionary[lang][wordKey];
+
+  let w: string = "";
+  const defLng = getDefaultLang();
+  if (!lang || defLng !== lang) {
+    const defaultLng = dictionary?.[defLng];
+    if (defaultLng?.[wordKey]) {
+      w = defaultLng[wordKey];
+    }
+  }
+
+  return w;
+}
+
+export class languageWords {
+  dictionary: { [x: string]: { [x: string]: string } };
+  lang: string;
+  constructor(opts: {
+    lang?: string;
+    dictionary: { [x: string]: { [x: string]: string } };
+  }) {
+    this.dictionary = opts.dictionary;
+    this.lang = opts.lang ? opts.lang : getDefaultLang();
+  }
+  setLang(lang: string) {
+    if (!lang) throw new Error("no lang provided");
+    this.lang = lang;
+  }
+  translate(wordKey: string) {
+    return getDictionaryWord(wordKey, this.dictionary, this.lang);
+  }
+}
