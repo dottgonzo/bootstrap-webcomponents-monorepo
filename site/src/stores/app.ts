6a7ce7fef3872e9dfa546e668ccfe430cd642ecc
dottg0n20 <dev@dariocaruso.info>
2022-03-22 00:14:19 +0100

    testing css vars inheritance

--- site/src/stores/app.ts
+++ site/src/stores/app.ts
@@ -1,6 +1,6 @@
 import { readable, writable, type Readable, type Writable } from 'svelte/store';
 
 export const pageName: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.7.2');
+export const componentsVersion: Readable<string> = readable('0.7.3');
 
 export const lang: Writable<string> = writable('');
