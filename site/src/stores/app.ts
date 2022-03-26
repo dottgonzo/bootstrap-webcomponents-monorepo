14037bf55c0f45b77f0669e8e49977b1b39a1b8e
dottg0n20 <dev@dariocaruso.info>
2022-03-26 02:29:37 +0100

    upgrade site

--- site/src/stores/app.ts
+++ site/src/stores/app.ts
@@ -1,6 +1,6 @@
 import { readable, writable, type Readable, type Writable } from 'svelte/store';
 
 export const pageName: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.8.16');
+export const componentsVersion: Readable<string> = readable('0.8.20');
 
 export const lang: Writable<string> = writable('');
