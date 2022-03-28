fa421a85ecde000131d3bf64cbff204fd5000647
Dario Caruso <darioyzf@gmail.com>
2022-03-28 11:23:49 +0200

    upp

--- site/src/stores/app.ts
+++ site/src/stores/app.ts
@@ -2,7 +2,7 @@ import { readable, writable, type Readable, type Writable } from 'svelte/store';
 import type allComponentsList from '@htmlbricks/hb-bundle/release/list.json';
 
 export const pageName: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.9.2');
+export const componentsVersion: Readable<string> = readable('0.9.3');
 export const debugVersion: Writable<string> = writable(null);
 
 export const homePage: Readable<string> = readable('/components/comparison');
