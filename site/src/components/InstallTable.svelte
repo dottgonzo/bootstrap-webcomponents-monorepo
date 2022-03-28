cc16546e9c4fc1e31d643825d59f960a097e9c8b
Dario Caruso <darioyzf@gmail.com>
2022-03-28 20:15:30 +0200

    move site on its own repo

--- site/src/components/InstallTable.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let cdn: string;
-	export let com: string;
-</script>
-
-<div><hb-area-code content={cdn} /></div>
-<div style="margin-top:20px"><hb-area-code content={com} /></div>
--- site/src/routes/components/main.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { pageName } from '../../stores/app';
-	pageName.set('components');
-</script>
-
-ss
