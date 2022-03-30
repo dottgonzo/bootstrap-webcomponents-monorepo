cec4e1a342cbc1c5da0e3804214a627c2e00645e
dottg0n20 <dev@dariocaruso.info>
2022-03-30 20:42:46 +0200

    add skeleton component

---
+++ packages/skeleton-component/app/types/webcomponent.type.d.ts
@@ -0,0 +1,9 @@
+export type Component = {
+	id?: string;
+	style?: string;
+	json?: json;
+	string: string;
+	boolean?: boolean;
+};
+
+export type Events = {};
---
+++ packages/skeleton-component/extra/docs.ts
@@ -0,0 +1,43 @@
+import type { CssPart, CssVar, HtmlSlot, i18nLang, StyleSetup, ComponentSetup } from "@htmlbricks/hb-jsutils/main";
+import type { Component } from "../app/types/webcomponent.type";
+
+export const storybookArgs = {
+	options: { control: { type: "object" } },
+};
+
+const cssVars: CssVar[] = [];
+export const cssParts: CssPart[] = [{ name: "gauge", description: "" }];
+export const htmlSlots: HtmlSlot[] = [];
+export const i18nLanguages: i18nLang[] = [];
+
+export const styleSetup: StyleSetup = {
+	vars: cssVars,
+	parts: cssParts,
+};
+
+const examples: Component[] = [
+	{
+		json: {
+			value: 50,
+			min: 0,
+			max: 100,
+		},
+		string: "test",
+		boolean: false,
+	},
+];
+export const componentSetup: ComponentSetup & { examples: Component[] } = {
+	definitions: null,
+	storybookArgs,
+	styleSetup,
+	htmlSlots,
+	i18n: i18nLanguages,
+	examples,
+	name: "hb-skeleton-component",
+	category: "dev",
+	tags: ["dev"],
+	size: {},
+	iifePath: "release/release.js",
+	repoName: "@htmlbricks/hb-skeleton-component",
+	version: null,
+};
