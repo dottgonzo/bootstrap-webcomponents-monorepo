cbeea946b0d3e07aeda704232e46ed73c756321b
Sazzad Hossain Khan <itsazzad@gmail.com>
2022-04-02 19:38:50 +0600

    ropdown-notifications first release

---
+++ packages/dropdown-notifications/app/html/item.svelte
@@ -0,0 +1,47 @@
+<svelte:options tag="hb-notification-item" />
+
+<script lang="ts">
+	export let avatar: string;
+	export let url: string;
+	export let text: string;
+	export let ext: string;
+
+	const isUrl = (string) => {
+		try {
+			return Boolean(new URL(string));
+		} catch (e) {
+			return false;
+		}
+	};
+	let hasAvatarUrl = isUrl(avatar);
+</script>
+
+<div class="dropdown-item py-3 d-flex">
+	{#if avatar}
+		<div>
+			{#if hasAvatarUrl}
+				<img alt="||" src={avatar} class="avatar rounded-circle" />
+			{:else}
+				<div class="avatar bg-success text-white rounded-circle">{avatar}</div>
+			{/if}
+		</div>
+	{/if}
+
+	<div class="flex-fill ms-3">
+		<div class="text-sm lg-snug w-64 text-wrap">
+			{#if url}
+				<a href={url}> <slot>{text}</slot> </a>
+			{:else}
+				<slot>{text}</slot>
+			{/if}
+		</div>
+		{#if $$slots.ext || ext}
+			<span class="text-muted text-xs"><slot name="ext">{ext}</slot></span>
+		{/if}
+	</div>
+</div>
+
+<style lang="scss">
+	@import "../styles/bootstrap.scss";
+	@import "../styles/item.scss";
+</style>
--- packages/dropdown-notifications/app/html/webcomponent.svelte
+++ packages/dropdown-notifications/app/html/webcomponent.svelte
@@ -3,60 +3,38 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
 	import { get_current_component } from "svelte/internal";
-	import debounce from "debounce";
 
-	$: {
-	}
+	export let clearUrl: string = $$props["clear-url"];
+	export let viewUrl: string = $$props["view-url"];
+	export let align: "left" | "right" = "left";
+
 	const component = get_current_component();
 	const svelteDispatch = createEventDispatcher();
 	function dispatch(name, detail) {
 		svelteDispatch(name, detail);
 		component.dispatchEvent && component.dispatchEvent(new CustomEvent(name, { detail }));
 	}
+	let show: boolean = false;
 </script>
 
 <div class="dropdown">
-	<a
-		href="#"
-		class="nav-link px-3 text-base text-muted text-opacity-70 text-opacity-100-hover show"
-		id="dropdown-notifications"
-		data-bs-toggle="dropdown"
-		aria-expanded="true"
+	<a on:click={() => (show = !show)} href="#" class:show class="nav-link px-3 text-base text-xs text-opacity-70 text-opacity-100-hover" aria-expanded={show}
 		><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
 			<path
 				d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"
 			/>
 		</svg></a
 	>
-	<div class="dropdown-menu dropdown-menu-end px-2 show" aria-labelledby="dropdown-notifications" data-bs-popper="none">
+	<div class:show class="dropdown-menu px-2" class:dropdown-menu-end={align === "right"} aria-labelledby="dropdown-notifications">
 		<div class="dropdown-item d-flex align-items-center">
 			<h6 class="dropdown-header p-0 m-0 font-semibold">Notifications</h6>
-			<a href="#" class="text-sm font-semibold ms-auto">Clear all</a>
+			{#if clearUrl}<a href={clearUrl} class="text-sm font-semibold ms-auto">Clear all</a>{/if}
 		</div>
-		<div class="dropdown-item py-3 d-flex">
-			<div>
-				<div class="avatar bg-success text-white rounded-circle">DC</div>
-			</div>
-			<div class="flex-fill ms-3">
-				<div class="text-sm lg-snug w-64 text-wrap">
-					<a href="#" class="font-semibold text-heading text-primary-hover">Dario</a>
-					sent a message to <a href="#" class="font-semibold text-heading text-primary-hover">HTMLBrick</a>
-				</div>
-				<span class="text-muted text-xs">30 mins ago</span>
-			</div>
-		</div>
-		<div class="dropdown-item py-3 d-flex">
-			<div><img alt="..." src="https://picsum.photos/200/300" class="avatar rounded-circle" /></div>
-			<div class="flex-fill ms-3">
-				<div class="text-sm lg-snug w-64 text-wrap">
-					<a href="#" class="font-semibold text-heading text-primary-hover">Dario</a>
-					sent a message to <a href="#" class="font-semibold text-heading text-primary-hover">HTMLBrick</a>
-				</div>
-				<span class="text-muted text-xs">30 mins ago</span>
-			</div>
-		</div>
-		<div class="dropdown-divider" />
-		<div class="dropdown-item py-2 text-center"><a href="#" class="font-semibold text-muted text-primary-hover">View all</a></div>
+		<slot />
+		{#if viewUrl}
+			<div class="dropdown-divider" />
+			<div class="dropdown-item py-2 text-center"><a href={viewUrl} class="font-semibold text-muted text-primary-hover">View all</a></div>
+		{/if}
 	</div>
 </div>
 
