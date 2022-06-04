a9416255a21cd8c6e1372125758ea65e4c26ed4d
dottgonzo <dev@dariocaruso.info>
2022-06-04 03:03:13 +0200

    wip

---
+++ storybook/stories/SiteSlideshow.stories.ts
@@ -0,0 +1,17 @@
+import type { Story } from "@storybook/html";
+import { webComponentBind, getStorybookMeta } from "./utils/webComponentUtils";
+import {
+  storybookArgs,
+  componentSetup,
+} from "../../packages/site-slideshow/extra/docs";
+
+const meta = getStorybookMeta(storybookArgs, componentSetup);
+export default meta;
+
+const Template: Story = (args) =>
+  webComponentBind(args, meta.argTypes, componentSetup.name);
+
+export const SiteSlideshowTemplate = Template.bind({});
+SiteSlideshowTemplate.args = {
+  id: "SiteSlideshowTemplate",
+};
