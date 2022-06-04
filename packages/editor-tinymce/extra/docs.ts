7ee93026e36decd7677f04edbe88bf7de760e8c3
dottgonzo <dev@dariocaruso.info>
2022-06-05 01:58:36 +0200

    rebuild

--- packages/editor-tinymce/extra/docs.ts
+++ packages/editor-tinymce/extra/docs.ts
@@ -18,7 +18,7 @@ export const styleSetup: StyleSetup = {
 	parts: cssParts,
 };
 
-const examples: Component[] = [{}];
+const examples: ComponentSetup["examples"] = [{ name: "default", data: {} }];
 export const componentSetup: ComponentSetup & { examples: { name: string; description?: string; data: Component }[] } = {
 	definitions: null,
 	storybookArgs,
