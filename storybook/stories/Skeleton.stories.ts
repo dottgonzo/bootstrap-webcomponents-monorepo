12581c01b7c129c719cd939e34f65248eacdecea
dottgonzo <dev@dariocaruso.info>
2022-06-05 03:07:32 +0200

    ref

--- storybook/stories/Skeleton.stories.ts
+++ storybook/stories/Skeleton.stories.ts
@@ -12,11 +12,11 @@ const Template: Story = (args) =>
   webComponentBind(args, meta.argTypes, componentSetup.name);
 
 export const Skel = Template.bind({});
-Skel.args = {
-  id: "Skel",
-  json: { a: 1 },
-  string: "ciao",
-  boolean: false,
-  _testInnerHtml:
-    '<span style="background-color:blue" slot="skelcontent">cooontent</span>',
-};
+Skel.args = Object.assign(
+  {
+    id: "Skel",
+    _testInnerHtml:
+      '<span style="background-color:blue" slot="skelcontent">cooontent</span>',
+  },
+  componentSetup.examples[0].data
+);
