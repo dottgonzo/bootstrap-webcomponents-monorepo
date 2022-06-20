9e51c6ee863527590f7d8d54dbba2706ee3ff403
dottgonzo <dev@dariocaruso.info>
2022-06-20 20:05:11 +0200

    start to work with site faq

---
+++ storybook/stories/SiteFaq.stories.ts
@@ -0,0 +1,22 @@
+import type { Story } from "@storybook/html";
+import {
+  webComponentBind,
+  getStorybookMeta,
+  setStorybookData,
+} from "./utils/webComponentUtils";
+import {
+  storybookArgs,
+  componentSetup,
+} from "../../packages/site-faq/extra/docs";
+
+const meta = getStorybookMeta(storybookArgs, componentSetup);
+export default meta;
+
+const Template: Story = (args) =>
+  webComponentBind(args, meta.argTypes as any, componentSetup.name);
+
+export const SiteFaq1 = Template.bind({});
+SiteFaq1.args = setStorybookData(
+  componentSetup.name,
+  componentSetup.examples[0]
+);
