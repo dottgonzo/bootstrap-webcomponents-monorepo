9ee730a22851683a6b63673ccd2fbf430b53d98a
dottgonzo <dev@dariocaruso.info>
2022-06-21 19:05:45 +0200

    add css parts on slideh

--- packages/site-slideshow-horizontal/extra/docs.ts
+++ packages/site-slideshow-horizontal/extra/docs.ts
@@ -9,8 +9,12 @@ export const storybookArgs = {
 };
 
 const cssVars: CssVar[] = [];
-export const cssParts: CssPart[] = [{ name: "testpart", description: "test css part on 2 div tag" }];
-export const htmlSlots: HtmlSlot[] = [{ name: "skelcontent", description: "test slot" }];
+export const cssParts: CssPart[] = [
+	{ name: "video_sub_title", description: "video_sub_title" },
+	{ name: "video_sub_title", description: "video_sub_title" },
+	{ name: "caption_content", description: "caption_content" },
+];
+export const htmlSlots: HtmlSlot[] = [];
 export const i18nLanguages: i18nLang[] = [];
 
 export const styleSetup: StyleSetup = {
