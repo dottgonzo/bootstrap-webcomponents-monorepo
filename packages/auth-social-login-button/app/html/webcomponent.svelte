32ca45e17a3d812405388b53bbb4d56761d8a70e
dottgonzo <dev@dariocaruso.info>
2022-07-16 03:31:59 +0200

    ref

--- packages/auth-social-login-button/app/html/webcomponent.svelte
+++ packages/auth-social-login-button/app/html/webcomponent.svelte
@@ -192,11 +192,6 @@
 	});
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-</svelte:head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-
 <div id="icon">
 	<div
 		id="icon-content"
