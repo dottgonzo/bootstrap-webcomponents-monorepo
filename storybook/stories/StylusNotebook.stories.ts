e2f47009619726b6a0e8f76fbdc08c191864fd13
dottg0n20 <dev@dariocaruso.info>
2022-12-10 13:32:28 +0100

    ref

--- storybook/stories/StylusNotebook.stories.ts
+++ storybook/stories/StylusNotebook.stories.ts
@@ -16,7 +16,4 @@ const Template: Story = (args) =>
   webComponentBind(args, meta.argTypes as any, componentSetup.name)
 
 export const Main = Template.bind({})
-Main.args = setStorybookData(componentSetup.name, componentSetup.examples[0], {
-  _testInnerHtml:
-    '<span style="background-color:blue" slot="skelcontent">cooontent</span>',
-})
+Main.args = setStorybookData(componentSetup.name, componentSetup.examples[0])
