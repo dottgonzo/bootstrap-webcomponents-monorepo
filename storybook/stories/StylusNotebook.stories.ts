321632fad4c4983ef320b546ad22450b281c13d9
dottg0n20 <dev@dariocaruso.info>
2022-12-18 13:17:17 +0100

    update storybook

--- storybook/stories/StylusNotebook.stories.ts
+++ storybook/stories/StylusNotebook.stories.ts
@@ -18,8 +18,8 @@ const Template: Story = (args) =>
 export const Main = Template.bind({})
 Main.args = setStorybookData(componentSetup.name, componentSetup.examples[0])
 
-export const WithLoad = Template.bind({})
-WithLoad.args = setStorybookData(
+export const WithLoadDraw = Template.bind({})
+WithLoadDraw.args = setStorybookData(
   componentSetup.name,
   componentSetup.examples[1],
 )
