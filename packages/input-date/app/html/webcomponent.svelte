54111ade96154a69450eee7829cbd49b153076dd
Dario Caruso <dev@dariocaruso.info>
2023-02-23 18:48:26 +0100

    uip

--- packages/input-date/app/html/webcomponent.svelte
+++ packages/input-date/app/html/webcomponent.svelte
@@ -10,7 +10,17 @@
 
 	export let schemaentry: FormSchemaEntry;
 
-	let value: string;
+		let value: string;
+
+	let value_date: string;
+	// let value_hours: number;
+	// let value_minutes: number;
+	// let value_seconds: number;
+
+	let schemaDate: FormSchemaEntry;
+	// let schemaHours: FormSchemaEntry;
+	// let schemaMinutes: FormSchemaEntry;
+	// let schemaSeconds: FormSchemaEntry;
 	// let regex: RegExp | undefined;
 	let valid = false;
 
@@ -38,29 +48,122 @@
 			set_valid = true;
 		}
 
-		value = value != null ? value : (schemaentry?.value as string);
 
-		// regex = schemaentry?.validationRegex && new RegExp(schemaentry.validationRegex);
-
-		if (schemaentry) {
-			if (schemaentry.required) {
-				if (value) {
-					if (schemaentry.params?.minLength && !(value.length >= schemaentry.params?.minLength)) {
-						valid = false;
-					} else if (schemaentry.params?.maxLength && !(value.length <= schemaentry.params.maxLength)) {
-						valid = false;
-					} else {
-						valid = true;
-					}
-				} else {
-					valid = true;
-				}
-			} else {
-				valid = false;
+		if (schemaentry?.value && (old_schemaentry_value !== schemaentry?.value || first_access)) {
+			console.info("computing values");
+			first_access = false;
+			let date: Date;
+			try {
+				date = new Date(schemaentry.value);
+			} catch (err) {
+				console.error("invalid input value on input-datetime", schemaentry.value);
 			}
-		} else {
-			valid = false;
+			if (date.valueOf() > 10000 && date.valueOf() < 10000000000) {
+				console.info("converting date");
+				value_date = date.toISOString().split("T")[0];
+				// value_hours = date.toISOString().split("T")[0];
+				// value_minutes = date.toISOString().split("T")[0];
+				// value_seconds = date.toISOString().split("T")[0];
+				schemaDate = {
+					id: "date",
+					type: "date",
+					value: value_date,
+					required: true,
+				};
+				schemaHours = {
+					id: "hours",
+					type: "number",
+					value: value_hours,
+					required: true,
+
+					params: {
+						min: 0,
+						max: 23,
+					},
+				};
+				schemaMinutes = {
+					id: "minutes",
+					type: "number",
+					value: value_minutes,
+					required: true,
+
+					params: {
+						min: 0,
+						max: 59,
+					},
+				};
+				schemaSeconds = {
+					id: "seconds",
+					type: "number",
+					value: value_seconds,
+					required: true,
+
+					params: {
+						min: 0,
+						max: 59,
+					},
+				};
+			} else {
+				console.error("invalid data value on input-datetime", value);
+			}
 		}
+		if (value_date && (value_hours || value_hours === 0) && (value_minutes || value_minutes === 0)) {
+			value = new Date(`${value_date} ${value_hours}:${value_minutes}:${value_seconds || 0}`).toISOString();
+			console.log("NOW", value_date, value_hours, value_minutes);
+		} else {
+			console.log("reset to 0 datetime");
+			value = null;
+			if (!schemaDate) {
+				schemaDate = {
+					id: "date",
+
+					type: "date",
+					required: true,
+				};
+			}
+			if (!schemaHours) {
+				schemaHours = {
+					id: "hours",
+
+					type: "number",
+					required: true,
+					params: {
+						min: 0,
+						max: 23,
+					},
+					placeholder: "hours",
+				};
+			}
+
+			if (!schemaMinutes) {
+				schemaMinutes = {
+					id: "minutes",
+
+					type: "number",
+					required: true,
+					params: {
+						min: 0,
+						max: 59,
+					},
+					placeholder: "minutes",
+				};
+			}
+			if (!schemaSeconds) {
+				schemaSeconds = {
+					id: "seconds",
+
+					type: "number",
+					required: true,
+					params: {
+						min: 0,
+						max: 59,
+					},
+					placeholder: "seconds",
+				};
+			}
+		}
+		old_schemaentry_value = schemaentry?.value;
+		// regex = schemaentry?.validationRegex && new RegExp(schemaentry.validationRegex);
 
 		setTimeout(() => {
 			if (set_value) dispatch("setValue", { value, id: schemaentry?.id });
