d7e740b88f28ea32147244899b7b9a41178a9ca8
dottg0n20 <dev@dariocaruso.info>
2023-02-24 20:01:56 +0100

    ref

--- storybook/stories/FormTextInput.stories.ts
+++ storybook/stories/FormTextInput.stories.ts
@@ -25,35 +25,41 @@ export const FormTextInputTemplate = Template.bind({});
 
 FormTextInputTemplate.args = setStorybookData(
   componentSetup.name,
-  componentSetup.examples[0]
+  componentSetup.examples.find((f) => f.name === "default") ||
+    (undefined as any)
 );
 export const FormTextInputTemplateRequired = Template.bind({});
 
 FormTextInputTemplateRequired.args = setStorybookData(
   componentSetup.name,
-  componentSetup.examples[1]
+  componentSetup.examples.find((f) => f.name === "defaultRequired") ||
+    (undefined as any)
 );
 export const FormTextInputTemplateRequiredWithMin = Template.bind({});
 
 FormTextInputTemplateRequiredWithMin.args = setStorybookData(
   componentSetup.name,
-  componentSetup.examples[2]
+  componentSetup.examples.find((f) => f.name === "requiredwithmin") ||
+    (undefined as any)
 );
 export const FormTextInputTemplateRequiredWithMax = Template.bind({});
 
 FormTextInputTemplateRequiredWithMax.args = setStorybookData(
   componentSetup.name,
-  componentSetup.examples[3]
+  componentSetup.examples.find((f) => f.name === "requiredwithmax") ||
+    (undefined as any)
 );
 export const FormTextInputTemplateRequiredWithMaxMin = Template.bind({});
 
 FormTextInputTemplateRequiredWithMaxMin.args = setStorybookData(
   componentSetup.name,
-  componentSetup.examples[4]
+  componentSetup.examples.find((f) => f.name === "requiredwithminmax") ||
+    (undefined as any)
 );
 export const FormTextInputTemplateRequiredWithRegex = Template.bind({});
 
 FormTextInputTemplateRequiredWithRegex.args = setStorybookData(
   componentSetup.name,
-  componentSetup.examples[5]
+  componentSetup.examples.find((f) => f.name === "requiredwithregex") ||
+    (undefined as any)
 );
